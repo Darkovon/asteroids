3d8015277a1ba47b5dd8bda1208115b9379b1b33
Darkovon <007search@gmail.com>
2025-03-12 17:53:47 -0500

    finished game basics

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
 import pygame
 from constants import *
+from player import *
+from asteroidfield import *
+import sys
+from shlex import split
 
 def main():
     pygame.init() # Initialize pygame
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Create a window with the specified dimensions
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updateables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    AsteroidField.containers = (updateables)
+    Player.containers = (updateables, drawables)
+    Asteroid.containers = (asteroids, updateables, drawables)
+    Shot.containers = (shots, updateables, drawables)
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updateables.update(dt)
+
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.split()
+
+        for drawable in drawables:
+            drawable.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+
+
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
